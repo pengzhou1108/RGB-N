983a207907a1fd333ede7245e7f49277091e4c49
Peng Zhou <peng@vulcansub00.umiacs.umd.edu>
2018-09-06 15:39:06 -0400

    sh file

--- test_dist_faster.sh
+++ test_dist_faster.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-#SBATCH -t 3:00:00
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH --gres=gpu
-#SBATCH --mem=30000
-#SBATCH -p gpu
-#SBATCH -g 1
-
-module add cuda/8.0.44 cudnn/v5.1
-module load Python3/3.5.2
 
 ./test_faster_rcnn.sh 0 coco res101_fusion EXP_DIR coco_flip_0001_bilinear_new
--- train_dist_faster.sh
+++ train_dist_faster.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-#SBATCH -t 20:00:00
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH --gres=gpu
-#SBATCH --mem=20000
-#SBATCH -p gpu
-module add cuda/8.0.44 cudnn/v5.1
-module load Python3/3.5.2
-#source /lustre/pengzhou/software/caffe_mine/load_dependency.sh
+
 ./train_faster_rcnn.sh 0 coco res101_fusion EXP_DIR coco_flip_0001_bilinear_1
