7589f1dd889d476fb2da71c84395056b28517234
Peng Zhou <peng@vulcansub00.umiacs.umd.edu>
2018-09-08 17:42:01 -0400

    bilinear

--- run_demo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-current_node=$SLURM_NODEID
-current_node=$(($current_node+1))
-n_begin=$1
-n_end=$2
-#source /lustre/pengzhou/software/caffe_mine/load_dependency.sh
-python demo.py --begin=$n_begin --end=$n_end
